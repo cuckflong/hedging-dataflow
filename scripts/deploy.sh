21f9ab630ded3709dfa1a61f3f7b67c1430de482
cuckflong <johnathanclong13@gmail.com>
2022-09-22 18:06:58 +0800

    service

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+$HOME/miniconda3/bin/activate prefect
+prefect config set PREFECT_API_URL=http://127.0.0.1:4200/api
+
+for f in ./deployments/*.py
+do
+    deploy=$(echo $f | cut -d '/' -f 3 | cut -d '.' -f1)
+    python -m deployments.$deploy
+done
