2d48bb950c2906fcd9a1c91f7dbd333c662e109d
cuckflong <johnathanclong13@gmail.com>
2022-09-27 05:26:29 +0800

    inted

--- flows/flow_db.py
+++ flows/flow_db.py
@@ -3,7 +3,7 @@ from prefect import flow, get_run_logger
 from tasks.task_db import drop_table
 
 
-@flow
+@flow(name="Drop table")
 def drop_table_flow(table_name: str):
     logger = get_run_logger()
     logger.info(f"Dropping table {table_name}")
