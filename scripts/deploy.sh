d9c446cc101488f5bc7dfa1a359da6b2daeb5482
cuckflong <johnathanclong13@gmail.com>
2022-09-27 18:15:54 +0800

    poetry

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-poetry shell
-poetry install
+/home/prefect/.local/bin/poetry shell
+/home/prefect/.local/bin/poetry install
 
 prefect config set PREFECT_API_URL=http://127.0.0.1:4200/api
 
--- scripts/start_prefect.sh
+++ scripts/start_prefect.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-poetry shell
-poetry install
+/home/prefect/.local/bin/poetry shell
+/home/prefect/.local/bin/poetry install
 
 prefect config set PREFECT_API_URL=https://prefect.cuckflong.io/api
 prefect orion start
